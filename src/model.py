d80ca3a4ddcc5a17cde133c842cb23c3b6603275
John Pertoft <john.pertoft@gmail.com>
2023-04-16 13:20:12 +0200

    Add missing regularization layers.

--- src/model.py
+++ src/model.py
@@ -21,18 +21,27 @@ class GPT(nn.Module):
     config: GPTConfig
 
     def setup(self):
+        # TODO:
+        # - Weight tying? Just need to pass the correct params during the apply I think?
+
         self.input_embeddings = nn.Embed(self.config.vocab_size, self.config.embed_dim)
         self.position_embeddings = nn.Embed(self.config.n_positions, self.config.embed_dim)
+        self.dropout = nn.Dropout(self.config.dropout)
         self.blocks = [GPTBlock(self.config) for _ in range(self.config.n_layers)]
+        self.ln = nn.LayerNorm()
         self.lm_head = nn.Dense(self.config.vocab_size, use_bias=False)
+        causal_mask = jnp.tril(jnp.ones((self.config.n_positions, self.config.n_positions), dtype="bool"))
+        causal_mask = rearrange(causal_mask, "i j -> 1 1 i j")
+        self.causal_mask = causal_mask
 
     def __call__(self, input_ids: jax.Array, *, train: bool):
         pos_ids = jnp.arange(input_ids.shape[1]).reshape((1, -1))
         x = self.input_embeddings(input_ids) + self.position_embeddings(pos_ids)
+        x = self.dropout(x, deterministic=not train)
         for block in self.blocks:
-            x = block(x, train=train)
+            x = block(x, causal_mask=self.causal_mask, train=train)
+        x = self.ln(x)
         logits = self.lm_head(x)
-        # TODO: There's some dropout and normalization missing here.
         return logits
 
 
@@ -42,16 +51,16 @@ class GPTBlock(nn.Module):
     def setup(self):
         self.ln1 = nn.LayerNorm()
         self.ln2 = nn.LayerNorm()
-        self.attention = CausalSelfAttention(self.config)
+        self.attention = SelfAttention(self.config)
         self.mlp = MLP(self.config)
 
-    def __call__(self, x: jax.Array, *, train: bool):
-        x = x + self.attention(self.ln1(x), train=train)
+    def __call__(self, x: jax.Array, *, causal_mask: jax.Array, train: bool):
+        x = x + self.attention(self.ln1(x), causal_mask=causal_mask, train=train)
         x = x + self.mlp(self.ln2(x), train=train)
         return x
 
 
-class CausalSelfAttention(nn.Module):
+class SelfAttention(nn.Module):
     config: GPTConfig
 
     def setup(self):
@@ -60,12 +69,13 @@ class CausalSelfAttention(nn.Module):
         self.wk = nn.Dense(self.config.embed_dim, use_bias=False)
         self.wv = nn.Dense(self.config.embed_dim, use_bias=False)
         self.wo = nn.Dense(self.config.embed_dim, use_bias=False)
-        # TODO: Can this be kept elsewhere instead?
-        causal_mask = jnp.tril(jnp.ones((self.config.n_positions, self.config.n_positions), dtype="bool"))
-        causal_mask = rearrange(causal_mask, "i j -> 1 1 i j")
-        self.causal_mask = causal_mask
+        self.attention_dropout = nn.Dropout(self.config.dropout)
+        self.residual_dropout = nn.Dropout(self.config.dropout)
 
-    def __call__(self, x: jax.Array, *, train: bool):
+    def __call__(self, x: jax.Array, *, causal_mask: jax.Array, train: bool):
+        # TODO:
+        # - Add support for attention masking of e.g. padded values.
+
         # Sequence length.
         t = x.shape[1]
 
@@ -79,8 +89,9 @@ class CausalSelfAttention(nn.Module):
         k = rearrange(k, "b l (h d) -> b l h d", h=self.config.n_heads)
         v = rearrange(v, "b l (h d) -> b l h d", h=self.config.n_heads)
 
-        # TODO: Rearrange in one step in the above instead?
-        # TODO: Or just use einsum?
+        # TODO:
+        # - Rearrange in one step in the above instead?
+        # - Or just use einsum?
         q = rearrange(q, "b l h d -> b h l d")
         k = rearrange(k, "b l h d -> b h l d")
         v = rearrange(v, "b l h d -> b h l d")
@@ -88,11 +99,10 @@ class CausalSelfAttention(nn.Module):
         # Compute attention.
         scaling = (1.0 / jnp.sqrt(k.shape[-1]))
         attention = q @ k.transpose(0, 1, 3, 2) * scaling
-        # TODO: Add mask to attention for padded values etc.
-        mask = self.causal_mask[..., :t, :t]
+        mask = causal_mask[..., :t, :t]
         attention = jnp.where(mask, attention, -jnp.inf)
         attention = nn.softmax(attention, axis=-1)
-        # TODO: Attention dropout here?
+        attention = self.attention_dropout(attention, deterministic=not train)
         output = attention @ v
         
         # Merge heads.
@@ -100,7 +110,7 @@ class CausalSelfAttention(nn.Module):
 
         # Compute output projection.
         output = self.wo(output)
-        # TODO: Residual dropout here?
+        output = self.residual_dropout(output, deterministic=not train)
 
         return output
 
