e8a5a5e81311cec420dd876397acdc52e0b47299
John Pertoft <john.pertoft@gmail.com>
2023-05-02 10:16:05 +0200

    Update model_test.py

--- src/model_test.py
+++ src/model_test.py
@@ -21,11 +21,12 @@ def test_self_attention_gradient_causality():
     )
     
     # Compute the gradients between all outputs and inputs and assert that outputs
-    # don't depend on inputs from the future.
+    # don't depend on inputs from the future. We run this in eval mode (train=False)
+    # so that the dropout layer don't interfere.
     f = lambda x: m.apply(params, x, causal_mask=causal_mask, train=False)
     g = jax.jacfwd(f)
     x = jax.random.normal(jax.random.PRNGKey(1), (1, 16, c.embed_dim))
     grads = g(x)
     grads = rearrange(grads, "1 t1 d1 1 t2 d2 -> t1 d1 t2 d2")
-    abs_grads_sum = jnp.abs(grads).sum(axis=(1, 3))  # Sum gradients for each embedding element.
+    abs_grads_sum = jnp.abs(grads).sum(axis=(1, 3))  # Sum gradients along embedding dim at each timestep.
     assert jnp.allclose(abs_grads_sum, jnp.tril(abs_grads_sum))
