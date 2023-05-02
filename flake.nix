b1c17e8d13d66584b43b12f9d5c79ceeff90f17c
John Pertoft <john.pertoft@gmail.com>
2023-05-02 22:15:12 +0200

    Inline dev shell.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,26 @@
     #pkgs = nixpkgs-unstable.legacyPackages.${system};
   in
   {
-    devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python3.withPackages (p: with p; [
+          datasets
+          einops
+          flax
+          ipython
+          jax
+          jaxlibWithCuda
+          matplotlib
+          numpy
+          optax
+          pytest
+          pyyaml
+          tokenizers
+          tqdm
+        ]))
+      ];
+    };
+    
     packages.${system}.default = pkgs.hello;
   };
 }
